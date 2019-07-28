a372165c47b16356ea97557f26eb3d1ce7aa8803
Wimpie Scholtz <wimpie.scholtz@iotnxt.com>
2019-07-28 20:53:36 +0200

    Added prod environment
    Forgot to add prod environment. Can now run the website in prod mode.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCk06JpDUe5caXD111xLqBiq7MTJzDZhdA",
+    authDomain: "regprog-u-1.firebaseapp.com",
+    databaseURL: "https://regprog-u-1.firebaseio.com",
+    projectId: "regprog-u-1",
+    storageBucket: "",
+    messagingSenderId: "87544251884"
+  }
 };
